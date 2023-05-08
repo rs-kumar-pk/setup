ab225c0d42fdf54d28c118f713d29719fc7bbc43
rs-kumar-pk <123354372+rs-kumar-pk@users.noreply.github.com>
2023-05-07 23:25:04 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,13 @@
+#!/bin/bash
+
 mkfs.ext4 /dev/sdc
+
 mkdir -p /data
 
 mount -t ext4 /dev/sdc /data
 
 timedatectl set-timezone Canada/Central
-sudo apt update -y
-sudo apt upgarde -y
+
+echo "Updating system..."
+sudo apt-get update -y
+sudo apt-get upgrade -y

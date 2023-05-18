2120941e5afffff936de914c13ba91b26501c7c1
rs-kumar-pk <123354372+rs-kumar-pk@users.noreply.github.com>
2023-05-18 11:33:07 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,13 +2,13 @@
 
 #!/bin/bash
 
-sudo mkfs.ext4 /dev/sdc
+sudo mkfs.efs4 /dev/sdc
 
 d=$(blkid -s UUID -o value /dev/sdc)
 
 sudo mkdir /data
 
-sudo su -c "echo UUID=$d /data ext4 defaults 0 0 >> /etc/fstab"
+sudo su -c "echo UUID=$d /data efs4 defaults 0 0 >> /etc/fstab"
 
 sudo mount /data
 

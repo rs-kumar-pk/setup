05265d26826c0b2b960d0c075f60ed60a08bc8e5
rs-kumar-pk <123354372+rs-kumar-pk@users.noreply.github.com>
2023-05-18 09:32:30 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,15 +1,19 @@
+
+
 #!/bin/bash
 
 sudo mkfs.ext4 /dev/sdc
 
+d=$(blkid -s UUID -o value /dev/sdc)
+
 sudo mkdir /data
 
-sudo su -c "echo '/dev/sdc /data ext4 defaults 0 0' >> /etc/fstab"
+sudo su -c "echo UUID=$d /data ext4 defaults 0 0 >> /etc/fstab"
 
 sudo mount /data
 
-sudo timedatectl set-timezone Canada/Eastern
+sudo timedatectl set-timezone Asia/Calcutta
 
 sudo echo "Updating system..."
 sudo apt-get update -y
-#sudo apt-get upgrade -y
+sudo apt-get upgrade -y

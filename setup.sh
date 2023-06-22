08dc4918bacef0bb883f66d8759375fe92e2ad50
rs-kumar-pk <123354372+rs-kumar-pk@users.noreply.github.com>
2023-06-22 10:58:18 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,15 +2,15 @@
 
 #!/bin/bash
 
-sudo mkfs.xfs /dev/sdc
+#sudo mkfs.xfs /dev/sdc
 
-d=$(blkid -s UUID -o value /dev/sdc)
+#d=$(blkid -s UUID -o value /dev/sdc)
 
-sudo mkdir /data
+#sudo mkdir /data
 
-sudo su -c "echo UUID=$d /data xfs defaults 0 0 >> /etc/fstab"
+#sudo su -c "echo UUID=$d /data xfs defaults 0 0 >> /etc/fstab"
 
-sudo mount /data
+#sudo mount /data
 
 sudo timedatectl set-timezone Canada/Eastern
 
